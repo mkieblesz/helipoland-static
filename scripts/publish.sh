dc459a445f6d437eb4ff2cb99941049b01a63aef
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2020-04-22 21:11:25 +0200

    Updated invalidation script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,9 @@
 aws s3 sync public/ s3://$AWS_S3_BUCKET --delete --acl public-read
 
 # refresh cloudfront cache (takes around 1 minute)
-invalidation_id=$(aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths "/*" | egrep Id | awk -F'"' '{ print $4}')
-echo "Waiting for $invalidation_id invalidation completion..."
-aws cloudfront wait invalidation-completed --id $invalidation_id --distribution-id $AWS_CF_DISTRIBUTION_ID
+aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths "/*"
+
+# you can also wait until invalidation is complete
+# invalidation_id=$(aws cloudfront create-invalidation --distribution-id $AWS_CF_DISTRIBUTION_ID --paths "/*" | egrep Id | awk -F'"' '{ print $4}')
+# echo "Waiting for $invalidation_id invalidation completion..."
+# aws cloudfront wait invalidation-completed --id $invalidation_id --distribution-id $AWS_CF_DISTRIBUTION_ID
